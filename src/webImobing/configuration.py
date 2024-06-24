6a805d6e19b87c50a6490b06c7abe6c68933ebc6
Eduardo de Moraes <eduardommfloripa1@gmail.com>
2024-06-24 20:12:28 -0300

    Refatora pequenas coisas

--- src/webImobing/configuration.py
+++ src/webImobing/configuration.py
@@ -6,12 +6,12 @@ class Configuration:
     def __init__(self, file_name: str):
         raw_config = FileHandler.load_settings(file_name)
 
-        self.scrapers = []
+        self.scraper_ids = []
         self.scraper_config = {}
         for key, value in raw_config.items():
             if key == "imobiliarias":
-                self.scrapers = value
+                self.scraper_ids = value
             else:
                 self.scraper_config[key] = value
 
-        logger.info(f"Configuração criada a partir do arquivo {file_name}")
+        logger.info(f"Os scrapers serão criados com a configuração extraída do arquivo {file_name}")
